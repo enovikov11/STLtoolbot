907fe372d291d68813e222728d4aa9e743f6419e
Evgenii Novikov <enovikov11@yandex.com>
2023-12-27 01:49:52 +0100

    mold WIP

--- src/tools.py
+++ src/tools.py
@@ -1,5 +1,6 @@
 from jsonrpc import JSONRPCResponseManager, dispatcher
 import socket
+import bmesh
 import bpy
 import os
 
@@ -91,6 +92,57 @@ def make_mold(obj, cover=5):
     z1, z2 = min(z) - cover, max(z) + cover
 
     return cube(x1, 0, y1, y2, z1, z2), cube(0, x2, y1, y2, z1, z2)
+
+
+@dispatcher.add_method
+def calculate_volume(obj):
+    bm = bmesh.new()
+    bm.from_mesh(obj.data)
+    bm.transform(obj.matrix_world)
+
+    volume = sum(f.calc_area() * f.normal.dot(f.verts[0].co) / 3 for f in bm.faces)
+    bm.free()
+    return abs(volume)
+
+
+@dispatcher.add_method
+def boolean(base, operand, operation):
+    assert operation in ['UNION', 'INTERSECT', 'DIFFERENCE']
+
+    modifier = base.modifiers.new(name='MyBoolean', type='BOOLEAN')
+    modifier.operation = operation
+    modifier.object = operand
+
+    bpy.context.view_layer.objects.active = base
+    bpy.ops.object.modifier_apply(modifier='MyBoolean')
+
+
+@dispatcher.add_method
+def sphere(x, y, z, d):
+    bpy.ops.mesh.primitive_ico_sphere_add(radius=d / 2, location=(x, y, z))
+    return bpy.context.object
+
+
+@dispatcher.add_method
+def delete(obj):
+    bpy.ops.object.select_all(action="DESELECT")
+    obj.select_set(True)
+    bpy.ops.object.delete()
+
+
+@dispatcher.add_method
+def cut_holes(base, left, right, small_size = 2, big_size = 2.4):
+    _, y, z = zip(*base.bound_box)
+
+    for fy, fz in [(min, min), (min, max), (max, min), (max, max)]:
+        small = sphere(0, fy(y), fz(z), small_size)
+        big = sphere(0, fy(y), fz(z), big_size)
+
+        boolean(left, big, "DIFFERENCE")
+        boolean(right, small, "UNION")
+
+        delete(small)
+        delete(big)
 
 
 def start_rpc(path):
